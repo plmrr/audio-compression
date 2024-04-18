81d82e03453c7ed658baedfb4be1bbec0afcb568
plmrr <aquaossa@gmail.com>
2024-04-18 15:00:02 +0200

    Added wave file input & output. Minor improvements.

--- main.py
+++ main.py
@@ -1,31 +1,26 @@
-from tools import generate_signal, measure_parameters, plot_signals, compress_signal, mean_squared_error
+from tools import generate_signal, measure_parameters, plot_signals, compress_signal, mean_squared_error, load_signal_from_file, save_compressed_signal
 from algorithms import dct_from_scratch, idct_from_scratch, dft, idft
 import numpy as np
 
 
 def main():
-    # Parameters
-    sample_rate = 1000  # samples per second
-    signal_type = 'square'  # or 'sine' 'triangle' 'square' 'sawtooth' 'random'
-    frequency = 200  # Hz, only used for wave
-    amplitude = 1  # Amplitude of the wave
-    method = 'DFT' # Method for processing signal
-    threshold_percent = 0.3 # Threshold for compression
-    
-    # Generate signal
-    signal = generate_signal(signal_type, frequency, amplitude, sample_rate)
-
-    # For later, now use constant values for testing
-    """
-    sample_rate = int(input("Please enter sample rate : "))
-    signal_type = input("Please enter signal type ('sine' 'triangle' 'square' 'sawtooth' 'random') : ")
-    frequency = int(input("Please enter frequency : "))
-    amplitude = int(input("Please enter amplitude : "))
-    method = input("Please choose method ('DCT', 'DFT'): ").upper()
-    threshold_percent = float(input("Please enter compression threshold in %: ")) / 100
-    """
+    choice = input("Do you want to 'load' a signal from a file or 'generate' one? (load/generate): ").lower()
+    if choice == 'generate':
+        sample_rate = int(input("Please enter sample rate : "))
+        signal_type = input("Please enter signal type ('sine' 'triangle' 'square' 'sawtooth' 'random') : ")
+        frequency = int(input("Please enter frequency : "))
+        amplitude = int(input("Please enter amplitude : "))
+        threshold_percent = float(input("Please enter compression threshold in %: ")) / 100
+        signal = generate_signal(signal_type, frequency, amplitude, sample_rate)
+    elif choice == 'load':
+        file_path = input("Please enter the path to the audio file: ")
+        sample_rate, signal = load_signal_from_file(file_path)
+        if len(signal.shape) == 2:
+            signal = signal.mean(axis=1)
 
     # Process signal
+    method = input("Please choose method ('DCT', 'DFT'): ").upper()
+    threshold_percent = float(input("Please enter compression threshold in %: ")) / 100
     if method == 'DCT':
         transformed_signal = dct_from_scratch(signal)
         threshold_value = threshold_percent * np.max(np.abs(transformed_signal))
@@ -36,7 +31,6 @@ def main():
         threshold_value = threshold_percent * np.max(np.abs(transformed_signal))
         compressed_signal = compress_signal(transformed_signal, threshold_value)
         reconstructed_signal = idft(compressed_signal)
-        pass
     else:
         raise ValueError("Invalid method! Available methods: 'DCT', 'DFT'.")
 
@@ -63,6 +57,9 @@ def main():
     # Plot the signals
     plot_signals(signal, transformed_signal, compressed_signal, reconstructed_signal, sample_rate, method)
 
+    # Save audio file if one was loaded
+    if choice == 'load':
+        save_compressed_signal('compressed_sample.wav', sample_rate, reconstructed_signal)
 
 if __name__ == "__main__":
     main()
--- tools.py
+++ tools.py
@@ -1,7 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import soundfile as sf
+import librosa
 
 
+# Generati
 def generate_signal(signal_type='sine', frequency=5, amplitude=1, sample_rate=100):
     t = np.linspace(0, 1, num=sample_rate, endpoint=False)
     if signal_type == 'sine':
@@ -27,31 +30,32 @@ def measure_parameters(signal):
 
 
 def plot_signals(original, transformed, compressed, reconstructed, sample_rate, method):
-    fig, axs = plt.subplots(2, 2, figsize=(12, 10))  # Ustawienie 4 wykresów w pionie
+    fig, axs = plt.subplots(2, 2, figsize=(12, 10))
     axs = axs.flatten()
 
-    t = np.linspace(0, 1, num=sample_rate, endpoint=False)  # Czas dla oryginalnego sygnału
+    t_original = np.linspace(0, len(original)/sample_rate, num=len(original), endpoint=False)
+    t_reconstructed = np.linspace(0, len(reconstructed)/sample_rate, num=len(reconstructed), endpoint=False)
 
-    # Wykres oryginalnego sygnału
-    axs[0].plot(t, original, linewidth=0.7)
+    # Original signal
+    axs[0].plot(t_original, original, linewidth=0.7)
     axs[0].set_title('Original Signal', fontsize=10)
     axs[0].set_xlabel('Time [s]', fontsize=8)
     axs[0].set_ylabel('Amplitude', fontsize=8)
 
-    # Wykres transformowanego sygnału (DCT/DFT)
+    # Transformed signal (DCT/DFT)
     axs[1].plot(np.abs(transformed) if method == 'DFT' else transformed)
     axs[1].set_title(f'{method} of the Signal')
     axs[1].set_xlabel('Frequency Bins', fontsize=8)
     axs[1].set_ylabel('Magnitude', fontsize=8)
 
-    # Wykres sygnału po kompresji
+    # Compressed signal
     axs[2].stem(np.abs(compressed) if method == 'DFT' else compressed)
     axs[2].set_title(f'{method} Spectrum after Compression', fontsize=10)
     axs[2].set_xlabel('Frequency Bins', fontsize=8)
     axs[2].set_ylabel('Magnitude', fontsize=8)
 
-    # Wykres sygnału po rekonstrukcji
-    axs[3].plot(t, reconstructed, linewidth=0.7)
+    # Reconstruced signal after compression
+    axs[3].plot(t_reconstructed, reconstructed, linewidth=0.7)
     axs[3].set_title('Reconstructed Signal', fontsize=10)
     axs[3].set_xlabel('Time [s]', fontsize=8)
     axs[3].set_ylabel('Amplitude', fontsize=8)
@@ -60,14 +64,37 @@ def plot_signals(original, transformed, compressed, reconstructed, sample_rate, 
     plt.show()
 
 
+# Compressing signal
+# A threshold specifies the minimum magnitude value that a signal component must have to be retained in the compressed signal.
 def compress_signal(transformed_data, threshold):
     compressed_data = transformed_data.copy()
     compressed_data[np.abs(compressed_data) < threshold] = 0
     return compressed_data
 
 
+# MSE
 def mean_squared_error(original, reconstructed):
     return np.mean((original - reconstructed) ** 2)
+
+
+# Loading wave file
+def load_signal_from_file(file_path):
+    signal, sample_rate = librosa.load(file_path, sr=None, mono=True)
+    return sample_rate, signal
+
+
+# Saving compressed signal
+def save_compressed_signal(file_path, sample_rate, reconstructed_signal):
+    max_val = np.max(np.abs(reconstructed_signal))
+    if max_val == 0:
+        print("Warning: Max value of the signal is 0. The file will contain only silence.")
+        reconstructed_signal_normalized = np.zeros_like(reconstructed_signal, dtype=np.int16)
+    else:
+        # 16-bit int normalization
+        reconstructed_signal_normalized = np.int16(reconstructed_signal / max_val * 32767)
+    
+    sf.write(file_path, reconstructed_signal_normalized, sample_rate)
+    print(f"Compressed audio has been saved to {file_path}")
 
 
 if __name__ == "__main__":
